fba2f80007e0b941ff85174398d19eece958b4b0
wxodi0 <teayang06@gmail.com>
2023-11-06 12:01:15 +0900

    Fix StyleButton for count Button

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,20 +1,30 @@
+import { useState } from 'react'
 import { ComponentMeta } from '@storybook/react'
 import { StyledButton } from '../components/StyleButton'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'StyledButton',
   component: StyledButton,
+  argTypes: { onClick: { action: 'clicked' } },
 } as ComponentMeta<typeof StyledButton>
 
-export const Primary = (props) => {
+const incrementAction = action('increment')
+
+export const Primary = (props : any) => {
+  const [count, setCount] = useState(0);
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
   return (
-    <StyledButton {...props} variant='primary'>
-      Primary
+    <StyledButton {...props} variant='primary' onClick={onClick}>
+      count : {count}
     </StyledButton>
   )
 }
 
-export const Success = (props) => {
+export const Success = (props : any) => {
   return (
     <StyledButton {...props} variant='success'>
       primary
@@ -22,7 +32,7 @@ export const Success = (props) => {
   )
 }
 
-export const Transparent = (porps) => {
+export const Transparent = (porps : any) => {
   return (
     <StyledButton {...porps} variant='transparent'>
       Transparent
